1a8aac4e67f55544346dd4b523992c0d9a446f56
BUSRA KUCUK <busra3590@gmail.com>
2023-04-23 13:53:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(a = "roller-skate"){
+    return `This Saturday, I want to ${a}!`;
+}
+
+const mondayWork = function(a = 'go to the office'){
+    return `This Monday, I will ${a}.`;
+}
+
+function wrapAdjective(name = "*"){
+    return function(adjective ="special"){
+        return `You are ${name}${adjective}${name}!`;
+    }
+}
